3315d736313942af24dbdb9039d28905bf15c107
Ajith kumar <ajith.kumar@travelxp.tv>
2020-10-27 06:28:05 +0530

    cant hide the tab navigator in specific tab

--- app/Router/router.js
+++ app/Router/router.js
@@ -13,52 +13,67 @@ import Account from '../Common/Account';
 const RootStackNavigator = createStackNavigator();
 const BottomNavigator = createBottomTabNavigator();
 
-export function Mainstack() {
-return(
-    <RootStackNavigator.Navigator
-        initialRouteName="HomeTab"
-    >
-        <RootStackNavigator.Screen name='React Navigation' component={HomeTab} />
-        <RootStackNavigator.Screen name='a' component={Stack} />
-        <RootStackNavigator.Screen name='Privacy' component={Privacy} />
-        <RootStackNavigator.Screen name='Tearms' component={Tarms} />
-        <RootStackNavigator.Screen name='Saved card' component={Savedcard} />
-        <RootStackNavigator.Screen name='Account' component={Account} />
+function getTabBarVisible(route) {
+    const routeName = route.state
+        ? route.state.routes[route.state.index].name
+        : route.params?.screen || 'Home';
 
-    </RootStackNavigator.Navigator>
-)}
+    if (routeName === 'Home') {
+        return true;
+    }
+    return false;
+}
 
-export function Stack(){
-    return(
+export function Mainstack({ navigation, route }) {
 
+    
+    return (
         <RootStackNavigator.Navigator
-        // initialRouteName="HomeTab"
-    >
-        {/* <RootStackNavigator.Screen name='React Navigation' component={Mainstack} /> */}
-        <RootStackNavigator.Screen name='Home' component={Home} />
-        <RootStackNavigator.Screen name='Search' component={Search} />
-        <RootStackNavigator.Screen name='Download' component={Download} />
-        <RootStackNavigator.Screen name='More' component={More} />
+            initialRouteName="HomeTab"
+        // screenOptions={{
+        //     headerShown: false
+        // }}
 
-    </RootStackNavigator.Navigator>
+        >
+            <RootStackNavigator.Screen name='React navigation' component={HomeTab} />
+            <RootStackNavigator.Screen name='Privacy' component={Privacy} />
+            <RootStackNavigator.Screen name='Tearms' component={Tarms} />
+            <RootStackNavigator.Screen name='Saved card' component={Savedcard} />
+            <RootStackNavigator.Screen name='Account' component={Account} />
+            <RootStackNavigator.Screen name='Download' component={Download} />
+            <RootStackNavigator.Screen name='More' component={More} />
+
+        </RootStackNavigator.Navigator>
     )
-} 
-export function HomeTab() {
-return(
+}
 
+export function HomeTab({ navigation, route }) {
+  
+    debugger
+    console.log(route);
+    if (route.state && route.state.index > 0) {
+        navigation.setOptions({ tabBarVisible: false })
+    } else {
+        navigation.setOptions({ tabBarVisible: true })
 
+    }              
 
-    <BottomNavigator.Navigator
-    // style={{borderwidth:2}}
-    initialRouteName='Home'
-    >
+    return (
+        <BottomNavigator.Navigator
+            initialRouteName='Home'
+        >   
+            <RootStackNavigator.Screen name='Home' component={Home} 
+            //options={({ route }) => ({
+            //     tabBarVisible: getTabBarVisible(route)
+            // })}
+            />
+            <RootStackNavigator.Screen name='Search' component={Search}
+            />
+            <RootStackNavigator.Screen name='Download' component={Download}
+            />
+            <RootStackNavigator.Screen name='More' component={More}
+            />
 
-
-
-        <RootStackNavigator.Screen name='Home' component={Home} />
-        <RootStackNavigator.Screen name='Search' component={Search} />
-        <RootStackNavigator.Screen name='Download' component={Download} />
-        <RootStackNavigator.Screen name='More' component={More} />
-
-    </BottomNavigator.Navigator>
-)}
+        </BottomNavigator.Navigator>
+    )
+}
